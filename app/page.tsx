447b20a184288efe1edc5e3038b21c11f01d9e59
Olaoluwa David Osikoya <osikoyaolaoluwa2021@gmail.com>
2025-05-27 11:44:54 +0100

    chore:add a bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/ui/Grid";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
+        {/* <FloatingNav
           navItems={[
             {
               name: "Home",
               link: "#home",
+              icon: <FaHome/>
             },
             {
               name: "About",
@@ -25,8 +28,9 @@ export default function Home() {
             },
           ]}
           className="hidden md:flex"
-        />
+        /> */}
         <Hero />
+        <Grid/>
       </div>
     </main>
   );
